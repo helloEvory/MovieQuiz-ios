be7ba11c01090ca1a433e1cbf7af20cf64d067e4
helloEvory <elijahegirev@gmail.com>
2023-02-09 21:39:39 +0300

    Добавлены общепринятые отступы

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -12,11 +12,11 @@ final class MovieQuizViewController: UIViewController {
     private var correctAnswers: Int = 0
     
     struct QuizQuestion {
-      let image: String
-      let text: String
-      let correctAnswer: Bool
+        let image: String
+        let text: String
+        let correctAnswer: Bool
     }
-
+    
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
@@ -59,25 +59,25 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
-        
+    
     // для состояния "Вопрос задан"
     struct QuizStepViewModel {
-      let image: UIImage
-      let question: String
-      let questionNumber: String
+        let image: UIImage
+        let question: String
+        let questionNumber: String
     }
-
+    
     // для состояния "Результат квиза"
     struct QuizResultsViewModel {
-      let title: String
-      let text: String
-      let buttonText: String
+        let title: String
+        let text: String
+        let buttonText: String
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let
-            firstQuestion = questions.first
+                firstQuestion = questions.first
         else {
             return
         }
@@ -98,53 +98,53 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func show(quiz step: QuizStepViewModel) {
-      // здесь мы заполняем нашу картинку, текст и счётчик данными
+        // здесь мы заполняем нашу картинку, текст и счётчик данными
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
-
+    
     private func show(quiz result: QuizResultsViewModel) {
-      // здесь мы показываем результат прохождения квиза
+        // здесь мы показываем результат прохождения квиза
         
         // создаём объекты всплывающего окна
         let alert = UIAlertController(
             title: result.title, // заголовок всплывающего окна
             message: result.text, // текст во всплывающем окне
             preferredStyle: .alert) // preferredStyle может быть .alert или .actionSheet
-
+        
         // создаём для него кнопки с действиями ("Сыграть ещё раз")
         let action = UIAlertAction(
             title: result.buttonText,
             style: .default,
             handler: { _ in
-            // вот этот код с переключением индекса и показом первого вопроса надо будет написать тут
-            self.currentQuestionIndex = 0
+                // вот этот код с переключением индекса и показом первого вопроса надо будет написать тут
+                self.currentQuestionIndex = 0
                 
-            // скидываем счётчик правильных ответов
-            self.correctAnswers = 0
+                // скидываем счётчик правильных ответов
+                self.correctAnswers = 0
                 
-            // заново показываем первый вопрос
-            let firstQuestion = self.questions[self.currentQuestionIndex]
-            let viewModel = self.convert(model: firstQuestion)
-            self.show(quiz: viewModel)
-        })
-
+                // заново показываем первый вопрос
+                let firstQuestion = self.questions[self.currentQuestionIndex]
+                let viewModel = self.convert(model: firstQuestion)
+                self.show(quiz: viewModel)
+            })
+        
         
         // добавляем в алерт кнопки
         alert.addAction(action)
-
+        
         // показываем всплывающее окно
         self.present(alert, animated: true, completion: nil)
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
-      // Попробуйте написать код конвертации сами
+        // Попробуйте написать код конвертации сами
         return QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
-      }
+    }
     
     private func showAnswerResult(isCorrect: Bool) {
         if isCorrect {
@@ -166,21 +166,21 @@ final class MovieQuizViewController: UIViewController {
         
         imageView.layer.borderColor = UIColor.clear.cgColor // очищаем цвет рамки перед следующим вопросом
         
-      if currentQuestionIndex == questions.count - 1 { // - 1 потому что индекс начинается с 0, а длинна массива — с 1
-        // показать результат квиза
-          let text = "Ваш результат: \(correctAnswers) из 10"
-          let viewModel = QuizResultsViewModel(
+        if currentQuestionIndex == questions.count - 1 { // - 1 потому что индекс начинается с 0, а длинна массива — с 1
+            // показать результат квиза
+            let text = "Ваш результат: \(correctAnswers) из 10"
+            let viewModel = QuizResultsViewModel(
                 title: "Этот раунд окончен!",
                 text: text,
                 buttonText: "Сыграть ещё раз")
-                show(quiz: viewModel)
-      } else {
-        currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
-        // показать следующий вопрос
-        let nextQuestion = questions[currentQuestionIndex]
-        let viewModel = convert(model: nextQuestion)
-                      
-        show(quiz: viewModel)
-      }
+            show(quiz: viewModel)
+        } else {
+            currentQuestionIndex += 1 // увеличиваем индекс текущего урока на 1; таким образом мы сможем получить следующий урок
+            // показать следующий вопрос
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
     }
 }
