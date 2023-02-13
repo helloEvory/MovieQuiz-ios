38b041b737cfd06ba7bf70523fc043a20d418319
helloEvory <elijahegirev@gmail.com>
2023-02-13 22:58:35 +0300

    Исправил бэкграунд

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,11 +2,10 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static var ypBlack: UIColor { UIColor(named: "ypBlack")! }
-    static var ypGray: UIColor { UIColor(named: "ypGray")! }
-    static var ypWhite: UIColor { UIColor(named: "ypWhite")! }
-    static var ypRed: UIColor { UIColor(named: "ypRed")! }
-    static var ypGreen: UIColor { UIColor(named: "ypGreen")! }
-    static var ypBackground: UIColor { UIColor(named: "ypBlackground")! }
+       static var ypBlack: UIColor { UIColor(named: "black") ?? UIColor.black }
+       static var ypGreen: UIColor { UIColor(named: "green") ?? UIColor.green }
+       static var ypRed: UIColor { UIColor(named: "red") ?? UIColor.red }
+       static var ypDarkGray: UIColor { UIColor(named: "darkGray") ?? UIColor.darkGray }
+       static var ypGray: UIColor { UIColor(named: "gray") ?? UIColor.gray }
+       static var ypWhite: UIColor { UIColor(named: "white") ?? UIColor.white }
 }
-
